094040d11ed6b0769f3437b6c9745229c76160df
lizy6 <lizy6@asiainfo.com>
2018-01-04 16:22:23 +0800

    商品列表页面完成01

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,9 +1,163 @@
 <template>
-  <div>I am goods</div>
+  <div class="goods">
+    <div class="menu-wrapper">
+      <ul>
+        <li v-for="item in goods" class="menu-item">
+          <span class="text border-1px">
+            <span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>{{item.name}}
+          </span>
+        </li>
+      </ul>
+    </div>
+    <div class="foods-wrapper">
+      <ul>
+        <li v-for="item in goods" class="food-list">
+          <h1 class="title">{{item.name}}</h1>
+          <ul>
+            <li v-for="food in item.foods" class="food-item">
+              <div class="icon">
+                <img width="57" height="57" :src="food.icon">
+              </div>
+              <div class="content">
+                <h2 class="name">{{food.name}}</h2>
+                <p class="desc">{{food.description}}</p>
+                <div class="extra">
+                  <span class="count">月售{{food.sellCount}}</span>
+                  <span>好评率{{food.rating}}%</span>
+                </div>
+                <div class="price">
+                  <span class="now_price">￥{{food.price}}</span>
+                  <span class="old_price" v-show="food.oldPrice">{{food.oldPrice}}</span>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+
 </template>
 <script type="text/ecmascript-6">
-  export default{};
+  const ERR_OK = 0;
+  export default {
+    props: {
+      seller: {
+        type: Object
+      }
+    },
+    data() {
+      return {
+        goods: []
+      }
+    },
+    created() {
+      this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
+      this.$http.get('/api/goods').then((response) => {
+        response = response.body;
+        if (response.errno === ERR_OK) {
+          this.goods = response.data;
+          //console.log(this.goods);
+        }
+      })
+    }
+  };
 </script>
 <style lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/stylus/mixin.styl"
+  .goods
+    display: flex
+    position: absolute
+    top: 174px
+    bottom: 46px
+    width: 100%
+    overflow: hidden
+    .menu-wrapper
+      flex: 0 0 80px
+      width: 80px
+      background: #f3f5f7
+      .menu-item
+        display: table
+        height: 54px
+        width: 56px
+        line-height: 14px
+        font-size: 12px
+        padding:0 12px
+        .icon
+          display: inline-block
+          width: 12px
+          height: 12px
+          vertical-align: top
+          margin-right: 2px
+          background-size: 12px 12px
+          background-repeat: no-repeat
+          &.decrease
+            bg-image('decrease_3')
+          &.discount
+            bg-image('discount_3')
+          &.guarantee
+            bg-image('guarantee_3')
+          &.invoice
+            bg-image('invoice_3')
+          &.special
+            bg-image('special_3')
+        .text
+          display: table-cell
+          font-size: 12px
+          width: 56px
+          vertical-align: middle
+          border-1px(rgba(7,17,27,0.1))
+    .foods-wrapper
+      flex: 1
+      .food-list
+        .title
+          border-left:2px solid #d9dde1
+          padding-left:14px
+          height: 26px
+          line-height:26px
+          width:100%
+          font-size: 12px
+          color:rgb(147,153,159)
+          background:#f3f5f7
+        .food-item
+          display:flex
+          margin:18px
+          padding-bottom:18px
+          border-1px(rgba(7,17,27,0.1))
+          &:last-child
+            border-none()
+            margin-bottom:0
+          .icon
+            flex:0 0 57px
+          .content
+            flex:1
+            padding-left:10px
+            .name
+              margin:2px 0 8px 0
+              font-size: 14px
+              line-height: 14px
+              color:rgb(7,17,27)
+            .desc
+              margin-bottom:8px
+              font-size: 10px
+              color:rgb(147,153,159)
+              line-height:10px
+            .extra
+              font-size: 10px
+              color:rgb(147,153,159)
+              line-height:10px
+              &.count
+                margin-right:12px
+            .price
+              font-weight:700
+              line-height:24px
+              .now_price
+                margin-right:8px
+                font-size: 14px
+                color:rgb(240,20,20)
+                font-weight:normal
+              .old_price
+                font-size: 10px
+                color:rgb(147,153,159)
 
 </style>
